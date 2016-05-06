96788d3d5c3ee429eed0bda8e02c5c4a6f19e48d
hao <hao@haodemacbook-pro.local>
2016-05-06 16:17:01 +0800

    Initial Commit

---
+++ SocketManger/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SocketManger
+//
+//  Created by hao on 16/5/6.
+//  Copyright © 2016年 hao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SocketManger/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SocketManger
+//
+//  Created by hao on 16/5/6.
+//  Copyright © 2016年 hao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
